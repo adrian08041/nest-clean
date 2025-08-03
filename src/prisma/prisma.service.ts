73f3f99600ead4b8ebe22e424810bf7a97d20cd8
Adrian Messias <adrianmessias62@gmail.com>
2025-08-03 14:04:09 -0300

    feat: added create account controller

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { on } from "events";
+import { PrismaClient } from "generated/prisma";
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      log: ["warn", "error"],
+    });
+  }
+
+  onModuleInit() {
+    return this.$connect();
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect();
+  }
+}
+
